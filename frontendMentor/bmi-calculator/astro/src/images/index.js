bc1b98e3de4ada283a773cb10436240689c8d8c5
Risk0-code <vincent.bouton.dev@gmail.com>
2023-12-18 12:28:16 +0100

    adding all text and some basic css

---
+++ frontendMentor/bmi-calculator/astro/src/images/index.js
@@ -0,0 +1,27 @@
+import iconAge from "../images/icon-age.svg";
+import iconEating from "../images/icon-eating.svg";
+import iconRace from "../images/icon-race.svg";
+import iconSleep from "../images/icon-sleep.svg";
+import iconGender from "../images/icon-gender.svg";
+import iconMuscle from "../images/icon-muscle.svg";
+import iconExercise from "../images/icon-exercise.svg";
+import iconPregnancy from "../images/icon-pregnancy.svg";
+import logo from "../images/logo.svg";
+import manEating from "../images/image-man-eating.webp";
+import curvedLineLeft from "../images/pattern-curved-line-left.svg";
+import curvedLineRight from "../images/pattern-curved-line-right.svg";
+
+export {
+  iconAge,
+  iconEating,
+  iconRace,
+  iconSleep,
+  iconGender,
+  iconMuscle,
+  iconExercise,
+  iconPregnancy,
+  logo,
+  manEating,
+  curvedLineLeft,
+  curvedLineRight,
+};
