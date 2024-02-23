ad6ece2a513799d4b9bf0da6090bfa4bc9de45d7
Vincent-Bouton <vincent.bouton.dev@gmail.com>
2024-02-23 13:20:32 +0100

    Removing Custom components, cause to break 3d

--- frontendMentor/space-tourism/astro.config.mjs
+++ frontendMentor/space-tourism/astro.config.mjs
@@ -5,14 +5,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue({
-    template: {
-      compilerOptions: {
-        // treat any tag that starts with ion- as custom elements
-        isCustomElement: (tag) => tag.startsWith('Tres'),
-      },
-    },
-  })],
+  integrations: [tailwind(), vue()],
   redirects: {
     '/destination': '/destination/moon',
     '/crew': '/crew/douglas-hurley',
