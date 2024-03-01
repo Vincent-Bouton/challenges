fc601ef55fac92dd15188a2ca7cd0f4056bd065a
Vincent-Bouton <vincent.bouton.dev@gmail.com>
2024-03-01 16:21:00 +0100

    Adding custom cursor

---
+++ frontendMentor/space-tourism/src/components/Cursor.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+let timeout;
+document.addEventListener('mousemove', function(e) {
+  const cursor = document.getElementById('cursor');
+  const after = document.getElementById('after');
+  cursor.style.left = e.pageX-5 + 'px';
+  cursor.style.top = e.pageY-5 + 'px';
+  const height = after.style.height === '' ? '30px': after.style.height;
+  // get just the number from height and width
+  const heightNumber = parseInt(height.replace(/\D/g, ''));
+
+  after.style.left = e.pageX-heightNumber/2 + 'px';
+  after.style.top = e.pageY-heightNumber/2 + 'px';
+
+});
+
+const hoverables = document.querySelectorAll('a, button');
+hoverables.forEach(element => {
+  element.addEventListener('mouseover', function(e) {
+    const after = document.getElementById('after');
+    after.style.width = '60px';
+    after.style.height = '60px';
+  });
+  element.addEventListener('mouseout', function(e) {
+    const after = document.getElementById('after');
+    after.style.width = '30px';
+    after.style.height = '30px';
+  });
+});
+
+</script>
+<template>
+  <div class="cursor">
+    <div id="cursor"></div>
+    <div id="after"></div>
+  </div>
+
+</template>
+<style>
+body {
+  cursor: none; /* Hide the default cursor */
+}
+
+a, button {
+  cursor: none;
+}
+
+#cursor, #after {
+  position: absolute;
+  border-radius: 50%;
+  mix-blend-mode: difference; /* Invert colors */
+  pointer-events: none; /* Ignore mouse events */
+  z-index: 9999; /* Make sure the custom cursor is on top of everything else */
+}
+
+#cursor {
+  background-color: white;
+  z-index: 10000;
+  height: 10px;
+  width: 10px;
+}
+
+#after {
+  height: 30px;
+  width: 30px;
+  background-color: rgba(255, 255, 255, 1);
+  z-index: 100;
+  transition: top 0.1s linear, left 0.1s linear, height .2s linear, width .2s linear; /* Add transition */
+}
+</style>
